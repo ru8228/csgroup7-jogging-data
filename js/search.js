f22c6c6fbf6780063e18d904f490f3eac765a142
kbjiyu <p98733211@gmail.com>
2016-12-17 10:57:08 +0800

    頁面顯示
    1.移動"amandahaha"的map search搜尋功能至 reseach.js。
    2.調整section about的顯示。
    2.1 待辦--用if判斷,鎖住hover的圖示。
    2.2 待辦--圖示設計。
    2.3 待辦--內容顯示設計。
    2.4 待辦--背景風格與特效。
    3.新增sidebar的mouseleave close。
    4.調整下方to-top button的顯示。
    4.1待辦--將圖示改為 往上衝刺的火箭!? 跑者!?

--- js/search.js
+++ js/search.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	var url="https://spreadsheets.google.com/feeds/list/1I2UEVDgoqUdLBVNCQUMcpq_k1oamSyzkkK7auiz1w2U/1/public/values?alt=json-in-script&callback=?";
-	var search_location;
-							
-	$("button").click(function(){
-		search_location=$("#search_location").val();
-		}),
-		$.getJSON(url+"&q"+search_location, function(json){
+function ShowValue() {
+	var v = document.getElementById("search_location").value;
+	if (v == "大安") {
+		v = "大安森林公園";
+	}
+	alert(v);
+	$.getJSON("https://spreadsheets.google.com/feeds/list/1I2UEVDgoqUdLBVNCQUMcpq_k1oamSyzkkK7auiz1w2U/1/public/values?alt=json-in-script&callback=?&q=" + document.getElementById("search_location").value, function(json) {
+		console.log("https://spreadsheets.google.com/feeds/list/1I2UEVDgoqUdLBVNCQUMcpq_k1oamSyzkkK7auiz1w2U/1/public/values?alt=json-in-script&callback=?&q=" + v);
 		var e = json.feed.entry,
-		l = e.length,
-		html = "",
-		entry, i, area, location, station, comment, temperature, rain, AQI, PM2, AQI_affect, PM2_5_affect
+			l = e.length,
+			html = "",
+			entry, i, area, location, station, comment, temperature, rain, AQI, PM2, AQI_affect, PM2_5_affect
 		for (i = 0; i < l; i++) {
 			entry = e[i];
 			area = entry.gsx$area.$t;
@@ -22,8 +22,9 @@ $(document).ready(function(){
 			pm2 = entry.gsx$pm2.$t;
 			aqiaffect = entry.gsx$aqiaffect.$t;
 			pm2affect = entry.gsx$pm2affect.$t;
-			html += area + " " + location + " " + station + " " + comment + " " + temperature  + " " + rain+ " " +aqi+" "+ pm2+ " " + aqiaffect+ " "+ pm2affect+"<br/>";
-			}
-		$("#info").html(html);
+			// html += area + " " + location + " " + station + " " + comment + " " + temperature  + " " + rain+ " " +aqi+" "+ pm2+ " " + aqiaffect+ " "+ pm2affect+"<br/>";
+			html += "<br/>" + "目前溫度: " + temperature + "<br/>" + "降雨機率: " + rain + "%" + "<br/>" + "AQI空氣汙染指標: " + aqi + aqiaffect + "<br/>" + "PM2.5: " + pm2 + pm2affect + "<br/><br/>跑友這麼說...<br/>" + comment + "let's run";
+		}
+		$("#test").html(html);
 	});
 }
